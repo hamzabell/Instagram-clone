5f3d6dbbe885e288b560dca4d0c2eb0deffd4db9
hamzabell <akanbassey8@gmail.com>
2024-12-23 23:36:09 +0100

    feat: 🎸 initialize app and added theme module

---
+++ src/theme/color.ts
@@ -0,0 +1,9 @@
+const colors = {
+	white: '#FFFFFF',
+	black: '#000000',
+	grey: '#666666',
+	lightgrey: '#cccccc',
+	primary: '#405DE6',
+}
+
+export default colors;
---
+++ src/theme/font.ts
@@ -0,0 +1,17 @@
+export const size = {
+	xs: 10,
+	sm: 12,
+	default: 14,
+	md: 16, 
+	lg: 20,
+	xl: 24,
+	xxl: 30
+}
+
+export const weight = {
+	full: '800', 
+	semi: '600', 
+	bold: 'bold',
+	normal: 'normal', 
+	thin: '400'
+}
